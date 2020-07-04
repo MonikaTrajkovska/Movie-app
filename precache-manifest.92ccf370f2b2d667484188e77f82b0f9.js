6b129d8ff0fa1639fe9af4b36e230111d786ff5b
MonikaTrajkovska <49168201+MonikaTrajkovska@users.noreply.github.com>
2020-07-04 15:18:39 +0200

    Updates

---
+++ precache-manifest.92ccf370f2b2d667484188e77f82b0f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc765f7110db93936004",
+    "url": "/Movie-app/static/css/main.468e4690.chunk.css"
+  },
+  {
+    "revision": "bc765f7110db93936004",
+    "url": "/Movie-app/static/js/main.bc765f71.chunk.js"
+  },
+  {
+    "revision": "6994553df0664269f7b6",
+    "url": "/Movie-app/static/js/1.6994553d.chunk.js"
+  },
+  {
+    "revision": "b860a52aeef2ba208ba7",
+    "url": "/Movie-app/static/js/runtime~main.b860a52a.js"
+  },
+  {
+    "revision": "b7df425dc1edb1862e2bb474c332e1ee",
+    "url": "/Movie-app/index.html"
+  }
+];
